2fbd6cd24af12229e9d28748a7d478a85e6126ae
davcs86 <davcs86@gmail.com>
2016-03-02 18:10:11 -0600

    Demo updated
    Update to use the last version of davcs86:bpmn-js#i18n

--- dist/app/components/viewer/viewerFactory.js
+++ dist/app/components/viewer/viewerFactory.js
@@ -3,9 +3,9 @@
   angular.module('custom-bpmnjs')
     .factory('viewerFactory', viewerFactory);
 
-  viewerFactory.$inject = ['$rootScope', 'diagramFactory', 'Viewer'];
+  viewerFactory.$inject = ['$rootScope', 'diagramFactory', 'Viewer', '$q', '$translate'];
 
-  function viewerFactory($rootScope, diagramFactory, Viewer) {
+  function viewerFactory($rootScope, diagramFactory, Viewer, $q, $translate) {
     var viewer = function() {
       var bpmnJS,
           that = this;
@@ -15,7 +15,15 @@
       $rootScope.$on('diagramImported', function(){
         that.loadFromDiagramFactory();
       });
+      $rootScope.$on('$translateChangeEnd', function () {
+        //$timeout(function () {
+        that.bpmnJS.get('translate').applyLanguage();
+        //});
+      });
       this.create();
+    };
+    viewer.prototype.translator = function(str, args){
+      return $translate.instant(str, args);
     };
     viewer.prototype.create = function(overwrite) {
       if (!!overwrite){
@@ -25,12 +33,19 @@
         container: '#viewer-canvas'
       });
       // assumes it's correct
-       this.loadFromDiagramFactory();
+      this.loadFromDiagramFactory();
     };
     viewer.prototype.loadFromDiagramFactory = function(){
       // assumes it's correct
-      var diagram = diagramFactory.get();
-      this.bpmnJS.importXML(diagram, angular.noop);
+      var diagram = diagramFactory.get(),
+          that = this;
+      this.bpmnJS.importXML(diagram, function(err){
+        if (err) {
+          return;
+        }
+        // set the translation function
+        that.bpmnJS.get('translate').t = that.translator;
+      });
     };
     viewer.prototype.downloadXML = function() {
       console.log("Download");
